4620588a88801da99725a069c2d07a91fa835d43
Marcin Stelmach <mstelmach@pgs-soft.com>
2017-11-14 14:13:01 +0100

    Added Tests

--- RepositoryApp.Test/Tests/UserServiceTest.cs
+++ RepositoryApp.Test/Tests/UserServiceTest.cs
@@ -12,24 +12,12 @@ using Xunit;
 
 namespace RepositoryApp.Test.Tests
 {
-    public class UserServiceShould
+    public class UserServiceTest
     {
         private readonly Mock<ApplicationDbContext> _dbContext;
         private readonly Mock<IConfiguration> _configuration;
         private readonly IUserService _userService;
 
-        public UserServiceShould()
-        {
-            _dbContext = new Mock<ApplicationDbContext>();
-            _configuration = new Mock<IConfiguration>();
-            _userService = new UserService(_dbContext.Object, _configuration.Object);
-        }
 
-        [Fact]
-        public async Task ReturnUsers()
-        {
-            var result = await _userService.GetUsers();
-            Assert.NotEmpty(result);
-        }
     }
 }
