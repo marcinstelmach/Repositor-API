3ed8c7612e54a8ed1e3aa20a28cc5f7ab271176d
Marcin Stelmach <mstelmach@pgs-soft.com>
2017-11-15 15:07:08 +0100

    Added service and DTO

---
+++ RepositoryApp.Service/Services/Implementations/VersionService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryApp.Service.Services.Implementations
+{
+    class VersionService
+    {
+    }
+}
---
+++ RepositoryApp.Service/Services/Interfaces/IVersionService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryApp.Service.Services.Interfaces
+{
+    public interface IVersionService
+    {
+        Task<Version> GetVersionByIdAsync(Guid versionId);
+        Task<IList<Version>> GetVersionsForUserAsync(Guid userId);
+        Task AddVersionAsync(Version version);
+        Task DeleteVersion(Version version);
+        Task<bool> SaveChangesAsync();
+    }
+}
