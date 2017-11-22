f54532fd50f04539dc5d2e62fa43d8957ca0e4e9
Marcin Stelmach <marcinxd4@gmail.com>
2017-11-22 17:53:33 +0100

    a

--- RepositoryApp.API/Startup.cs
+++ RepositoryApp.API/Startup.cs
@@ -83,7 +83,7 @@ namespace RepositoryApp.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext dbContext)
         {
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
@@ -98,7 +98,7 @@ namespace RepositoryApp.API
             app.UseMvc();
             
 
-            //DbInitializeProvider.InitializeWithDefaults(dbContext);
+            DbInitializeProvider.InitializeWithDefaults(dbContext);
         }
     }
 }
